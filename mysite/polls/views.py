6ea83eee66e5fbc041aceb8213be5f3d0580ce28
dennisnderitu254 <dknderitu@gmail.com>
2019-05-10 11:30:18 +0300

    debugging views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -29,7 +29,7 @@ def vote(request, question_id):
 	# return HttpResponse("You're voting on question %s." % question_id)
 	question = get_object_or_404(Question, pk=question_id)
 	try:
-		selected_choice = question.choice_set(pk=request.POST['choice'])
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist):
 		# Redisplay the question voting form.
 		return render(request, 'polls/details.html' ,{
