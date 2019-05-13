8a24343bb1f55ec238aa7537efea665b0bfbbd54
dennisnderitu254 <dknderitu@gmail.com>
2019-05-13 15:34:36 +0300

    customizing the app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -22,10 +22,11 @@ class DetailView(generic.DetailView):
 	template_name = 'polls/detail.html'
 
 	def get_queryset(self):
-        # Excludes any questions that aren't published yet.
-        return Question.objects.filter(pub_date__lte=timezone.now())
+		return Question.objects.filter(pub_date__lte=timezone.now())
+       
 
-        
+
+         # Excludes any questions that aren't published yet.
         
 class ResultsView(generic.DetailView):
 	model = Question
